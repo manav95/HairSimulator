091bf8ab3cb7b1099ecb1a66d10819e4fad0fd8f
manav95 <manavbiodutta@yahoo.com>
2014-08-10 00:38:40 -0400

    Githubs

--- src/com/example/hairsimulator/HairDrawer.java
+++ src/com/example/hairsimulator/HairDrawer.java
@@ -9,39 +9,53 @@ import static org.bytedeco.javacpp.opencv_highgui.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 import com.example.hairsimulator.model.ClassOval;
-
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.widget.ImageView;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.MatOfRect;
 
 public class HairDrawer {
 	 private Canvas canvas;
 	 private Paint paint;
 	 private ClassOval oval;
 	 private Bitmap bit;
-	 private IplImage image;
+	 private Mat mat;
      public HairDrawer(Canvas c, Paint p, ClassOval o, Bitmap bitmap) {
     	  canvas = c;
     	  paint = p;
     	  oval = o;
     	  this.bit = bitmap;
-    	  image = IplImage.create(bit.getWidth(), bit.getHeight(), IPL_DEPTH_8U, 4);
-    	  bit.copyPixelsToBuffer(image.getByteBuffer());
+    	  mat = new Mat();
+    	  Utils.bitmapToMat(bit, mat);
      }
      public void drawHairOval() {
     	  int [][] points = oval.getPoints();
-    	  CvPoint p1 = new CvPoint();
-    	  CvPoint p2 = new CvPoint();
-          p1.x(points[0][0]);
-          p1.y(points[0][1]);
-          p2.x(points[3][0]);
-          p2.y(points[3][1]);
-          cvRectangle(image, p1, p2, CV_RGB(255, 0, 0), 2, 8, 0);
-          ImageView view = (CustView) MainActivity.getActivity().findViewById(R.id.imageView1);
-          bit.copyPixelsFromBuffer(image.getByteBuffer());  
+    	  Point p1 = new Point();
+    	  Point p2 = new Point();
+          p1.x = points[0][0];
+          p1.y = points[0][1];
+          p2.x = points[3][0];
+          p2.y = points[3][1];
+          int height = mat.height();
+          int width = mat.width();
+          for (int i = (int) p1.x; i < (int) p2.x; i++) {
+             for (int j = (int) p1.y; j < (int) p2.y; j++) {
+                 double [] data = mat.get(i,j);
+                 data[0] = 255;
+                 data[1] = 255;
+                 data[2] = 255;
+                 mat.put(i, j, data);
+             }
+          }
+          Utils.matToBitmap(mat, bit);
+          ImageView view = (CustView) MainActivity.getActivity().findViewById(R.id.imageView1); 
           view.setImageBitmap(bit);
 	      ((CustView) view).setBits(bit);
     	  
--- src/com/example/hairsimulator/MainActivity.java
+++ src/com/example/hairsimulator/MainActivity.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,6 +27,18 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     private ImageView view;
     private static Activity myActivity;
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:   Log.i("OpenCV", "OpenCV loaded successfully");
+                                                        break;
+                default:  super.onManagerConnected(status);
+                          break;
+            }
+        }
+    };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,21 +67,30 @@ public class MainActivity extends Activity {
 	          }
 	        }
 	    }
-
-
-	}
+    }
+	
     public static Activity getActivity() {
     	return myActivity;
     }
-
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("Attitude", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, this, mLoaderCallback);
+        }
+        else {
+            Log.d("Attitude", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	public void startUp(View v) {
 		view.setOnTouchListener(new JobListener(true)); 
 	}
-	
-
-}
+} 
